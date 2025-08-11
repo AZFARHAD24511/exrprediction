ac964c4db9f8b9497e41a34fe0c8c882a3163606
Azizallah Farhadi <115372006+AZFARHAD24511@users.noreply.github.com>
2025-08-12 00:44:54 +0200

    Update decorators.py

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,23 +1,18 @@
-import time
-import logging
+import time, logging
 from functools import wraps
 
-# تنظیم لاگر برای ذخیره و نمایش خطاها
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def timer(func):
-    """دکوراتور برای محاسبه زمان اجرای تابع"""
     @wraps(func)
     def wrapper(*args, **kwargs):
-        start_time = time.time()
+        start = time.time()
         result = func(*args, **kwargs)
-        elapsed_time = time.time() - start_time
-        logging.info(f"{func.__name__} executed in {elapsed_time:.4f} seconds")
+        logging.info(f"{func.__name__} executed in {time.time()-start:.3f}s")
         return result
     return wrapper
 
 def error_handler(func):
-    """دکوراتور برای گرفتن خطا و جلوگیری از کرش کردن برنامه"""
     @wraps(func)
     def wrapper(*args, **kwargs):
         try:
