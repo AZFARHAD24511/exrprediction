ec0fe1a0544e1eb2c95cd1ce6ca644fc210c553f
Azizallah Farhadi <115372006+AZFARHAD24511@users.noreply.github.com>
2025-08-11 22:46:22 +0200

    Update forecasting.py

--- model/forecasting.py
+++ model/forecasting.py
@@ -1,44 +1,40 @@
-import pandas as pd
 import numpy as np
-from datetime import timedelta
-from sklearn.preprocessing import StandardScaler
-from config import MODEL_CONFIG, N_LAGS, TEST_SIZE
-from model.model_evaluation import evaluate_model
-from utils.decorators import error_handler
+import pandas as pd
+import sklearn
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 
-@error_handler
-def prepare_features(df, n_lags=N_LAGS):
-    lagged = {}
-    for lag in range(1, n_lags + 1):
-        lagged[f'price_lag{lag}'] = df['price'].shift(lag)
-        for kw in df.columns.drop('price'):
-            lagged[f'{kw}_lag{lag}'] = df[kw].shift(lag)
+from utils.decorators import timer
 
-    lag_df = pd.DataFrame(lagged, index=df.index).dropna()
-    full_df = pd.concat([df[['price']].iloc[n_lags:], lag_df.iloc[n_lags:]], axis=1)
-    return full_df, lag_df.columns.tolist()
-
-@error_handler
-def train_models(X_train, y_train):
-    results = {}
-    for name, config in MODEL_CONFIG.items():
-        model_class = eval(config['class'])
-        model = model_class(**config['params'])
+@timer
+def train_models(X_train, y_train, config):
+    models = {}
+    for name, params in config['models'].items():
+        model_class = None
+        if params['class'] == 'RandomForestRegressor':
+            model_class = RandomForestRegressor
+        elif params['class'] == 'LinearRegression':
+            model_class = LinearRegression
+        else:
+            raise ValueError(f"Unknown model class: {params['class']}")
+        
+        model = model_class(**params.get('params', {}))
         model.fit(X_train, y_train)
-        results[name] = model
+        models[name] = model
+    return models
+
+@timer
+def evaluate_models(models, X_test, y_test):
+    results = {}
+    for name, model in models.items():
+        preds = model.predict(X_test)
+        mse = np.mean((preds - y_test) ** 2)
+        results[name] = {'mse': mse}
     return results
 
-@error_handler
-def build_next_features(base_df, features, n_lags=N_LAGS):
-    next_vals = {}
-    for feat in features:
-        if feat.startswith('price_lag'):
-            lag = int(feat.split('lag')[-1])
-            next_vals[feat] = base_df['price'].iloc[-lag]
-        elif '_lag' in feat:
-            kw, _, lagstr = feat.rpartition('_lag')
-            lag = int(lagstr)
-            next_vals[feat] = base_df[kw].iloc[-lag] if kw in base_df else np.nan
-        else:
-            next_vals[feat] = np.nan
-    return pd.DataFrame([next_vals])
+@timer
+def predict(models, X):
+    predictions = {}
+    for name, model in models.items():
+        predictions[name] = model.predict(X)
+    return predictions
