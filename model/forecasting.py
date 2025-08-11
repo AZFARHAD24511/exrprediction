23d26b53bd319df48c6fcd32397ca86b23121761
Azizallah Farhadi <115372006+AZFARHAD24511@users.noreply.github.com>
2025-08-11 22:49:54 +0200

    Update forecasting.py

--- model/forecasting.py
+++ model/forecasting.py
@@ -1,23 +1,24 @@
 import numpy as np
 import pandas as pd
-import sklearn
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 
 from utils.decorators import timer
 
+# دیکشنری نگاشت نام کلاس به کلاس پایتون
+MODEL_MAP = {
+    'RandomForestRegressor': RandomForestRegressor,
+    'LinearRegression': LinearRegression
+}
+
 @timer
 def train_models(X_train, y_train, config):
     models = {}
     for name, params in config['models'].items():
-        model_class = None
-        if params['class'] == 'RandomForestRegressor':
-            model_class = RandomForestRegressor
-        elif params['class'] == 'LinearRegression':
-            model_class = LinearRegression
-        else:
-            raise ValueError(f"Unknown model class: {params['class']}")
-        
+        model_name = params['class']
+        if model_name not in MODEL_MAP:
+            raise ValueError(f"Unknown model class: {model_name}")
+        model_class = MODEL_MAP[model_name]
         model = model_class(**params.get('params', {}))
         model.fit(X_train, y_train)
         models[name] = model
